bd370081d1f1b10dc3edd66bcf11ac865c7aeeb9
Ryn830 <ryn830@gmail.com>
2021-10-17 12:07:01 -0700

    Initial commit w/ basic template

---
+++ src/app.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react'
+import { render } from 'react-dom'
+
+import './index.css'
+
+const App = () => <div className='w-screen h-screen p-10 bg-blue-500'>App</div>
+
+render(<App />, document.getElementById('root'))
---
+++ tailwindcss.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  mode: 'jit',
+  purge: ['./src/*.{html,js,jsx,ts,tsx}'],
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+}
